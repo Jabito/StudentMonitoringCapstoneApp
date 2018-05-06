bb96ede15124fb625a76135c120dce04e4ca9f30
unknown <jjosephhci1@gmail.com>
2018-05-06 18:06:27 +0800

    retesting firebase notif

--- app/src/main/java/com/capstone/mapua/studentmonitoringapp/firebase/MessagingService.java
+++ app/src/main/java/com/capstone/mapua/studentmonitoringapp/firebase/MessagingService.java
@@ -58,7 +58,6 @@ public class MessagingService extends FirebaseMessagingService {
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
-
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(this)
